ed1d9b424f57d25fe5713551fe1c6896612faaf4
IvaTsu <ivan.tsud@gmail.com>
2018-03-06 21:34:31 +0200

    setting state on input change

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,20 +8,16 @@ class SearchBar extends Component {
 		};
 	}
 
-	_handleSearchBarInputChange = (event) => {
-		const x = event.target.value;
-		console.log(x);
-		this.setState({ searchRequest: x });
-		console.log(this.state.searchRequest);
-	}
-
 	render() {
 		return (
-			<input
-  type="text"
-  placeholder="Search ..."
-  onChange={event => this._handleSearchBarInputChange(event)}
-			/>
+			<div>
+				<input
+  				type="text"
+  				placeholder="Search ..."
+  				onChange={event => this.setState({ searchRequest: event.target.value })}
+				/>
+				Input value: {this.state.searchRequest}
+			</div>
 		);
 	}
 }
